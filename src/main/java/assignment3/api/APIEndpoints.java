3ed7ea47637d769f0097d35cca593af3ed7da24f
Evi Lusiany <evi.lusiany@gmail.com>
2021-07-04 01:17:21 +0700

    final assignment

---
+++ src/main/java/assignment3/api/APIEndpoints.java
@@ -0,0 +1,11 @@
+package assignment3.api;
+
+public final class APIEndpoints {
+
+	public final static String loginAPI = "/users/sign_in";
+	public final static String dashboardAPI = "/build_cards";
+	public final static String signUpAPI = "/users";
+	public final static String profileAPI = "/user";
+	public final static String logOutAPI = "/users/sign_out";
+
+}
---
+++ src/test/java/assignment3/api/APITests.java
@@ -0,0 +1,69 @@
+package assignment3.api;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import com.google.gson.internal.LinkedTreeMap;
+
+import assignment3.api.utils.DataUtils;
+import assignment3.api.utils.TestUtils;
+import io.restassured.response.Response;
+
+public class APITests extends BaseAPITest {
+
+	String authToken = "token not found";
+	String Email = null;
+	String Message = "Logged out successfully";
+
+	@Test(priority = 1)
+	public void signUpAPI() {
+
+		LinkedTreeMap<String, Object> signUpMap = TestUtils.convertJsonToMap(DataUtils
+				.getDataFromExcel("Payload", "SignUpAPI").replace("uniqueEmail", faker.name().username() + "@gmail.com")
+				.replace("uniquePhone", "+62-83803072101"));
+
+		Response responseSignUpAPI = given().spec(commonSpec).body(signUpMap).when().post(APIEndpoints.signUpAPI);
+
+		verifyAPIStatusTimeAndHeader(responseSignUpAPI, 200);
+
+		authToken = getDataFromResponseUsingJsonPath(responseSignUpAPI, JsonPaths.authToken);
+		System.out.println(authToken);
+
+		Email = getDataFromResponseUsingJsonPath(responseSignUpAPI, JsonPaths.email);
+		System.out.println(Email);
+
+	}
+
+	@Test(priority = 2)
+	public void profileAPI() {
+
+		Response responseProfileAPI = given().spec(commonSpec).header("authtoken", authToken).when()
+				.get(APIEndpoints.profileAPI);
+
+		System.out.println(authToken);
+
+		verifyAPIStatusTimeAndHeader(responseProfileAPI, 200);
+
+		String verifyEmail = getDataFromResponseUsingJsonPath(responseProfileAPI, "email");
+		AssertJUnit.assertEquals(verifyEmail, Email);
+
+	}
+
+	@Test(priority = 3)
+	public void logOutAPI() {
+
+		Response responseLogOutAPI = given().spec(commonSpec).param("authtoken", authToken).when()
+				.delete(APIEndpoints.logOutAPI);
+
+		System.out.println(authToken);
+
+		verifyAPIStatusTimeAndHeader(responseLogOutAPI, 200);
+
+		String verifyResponseMessage = getDataFromResponseUsingJsonPath(responseLogOutAPI, "message");
+		AssertJUnit.assertEquals(verifyResponseMessage, Message);
+
+	}
+
+}
